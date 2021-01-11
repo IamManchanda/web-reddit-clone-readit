725af7f534caf13ef263bd75b61bb0de2ed73e6d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-12 01:02:31 +0530

    More progress in register ui design: form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Register() {
   return (
@@ -14,6 +15,56 @@ export default function Register() {
           backgroundImage: "url('/images/bricks.jpg')",
         }}
       ></div>
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-72">
+          <h1 className="mb-2 text-lg font-medium">Sign up</h1>
+          <p className="mb-10 text-xs">
+            By continuing, you agree to our User Agreement and Privacy Policy.
+          </p>
+          <form>
+            <div className="mb-6">
+              <input
+                type="checkbox"
+                className="mr-1 cursor-pointer"
+                id="agreement"
+              />
+              <label htmlFor="agreement" className="text-xs cursor-pointer">
+                I agree to get emails about cool stuff on Readit.
+              </label>
+            </div>
+            <div className="mb-2">
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="text"
+                placeholder="Password"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+              />
+            </div>
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+              Sign Up
+            </button>
+          </form>
+          <small>
+            Already a Readitor?
+            <Link href="/login">
+              <a className="ml-1 text-blue-500 uppercase">Log In</a>
+            </Link>
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
