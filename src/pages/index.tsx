980cec4200d3104c655d0cb527d433c224e4353c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-12 20:12:54 +0530

    Add form search input in navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,13 @@ export default function Home() {
               </Link>
             </span>
           </div>
+          <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+            <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+            <input
+              type="text"
+              className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+            />
+          </div>
         </div>
       </div>
     </Fragment>
