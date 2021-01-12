fd10480815e1f936b4d0feb01c406ad199584b12
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-12 22:00:00 +0530

    Make Navbar reusable within whole app
    This also restricts navbar to show up in auth routes as intented!

---
+++ src/components/navbar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import RedditLogo from "../assets/images/reddit.svg";
+
+const Navbar: React.FC = () => (
+  <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
+    <div className="flex items-center">
+      <Link href="/">
+        <a>
+          <RedditLogo className="w-8 h-8 mr-2" />
+        </a>
+      </Link>
+      <span className="text-2xl font-semibold">
+        <Link href="/">
+          <a>readit</a>
+        </Link>
+      </span>
+    </div>
+    <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+      />
+    </div>
+    <div className="flex">
+      <Link href="/login">
+        <a className="w-32 py-1 mr-4 leading-5 button blue hollow">Log In</a>
+      </Link>
+      <Link href="/register">
+        <a className="w-32 py-1 leading-5 button blue">Sign Up</a>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import { AppProps } from "next/app";
 import axios from "axios";
 import "../assets/styles/scss/tailwind.scss";
+import { Fragment } from "react";
+import Navbar from "../components/navbar";
+import { useRouter } from "next/router";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const isAuthRoute = authRoutes.includes(pathname);
+  return (
+    <Fragment>
+      {!isAuthRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default App;
