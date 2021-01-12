03a9e672c00570d4eac0534931c4e01d5365f480
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-12 21:42:16 +0530

    Login & Sign Up Buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,27 +9,33 @@ export default function Home() {
       <Head>
         <title>Readit: The Front Page of the Internet</title>
       </Head>
-      <div className="">
-        <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
-          <div className="flex items-center">
+      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
+        <div className="flex items-center">
+          <Link href="/">
+            <a>
+              <RedditLogo className="w-8 h-8 mr-2" />
+            </a>
+          </Link>
+          <span className="text-2xl font-semibold">
             <Link href="/">
-              <a>
-                <RedditLogo className="w-8 h-8 mr-2" />
-              </a>
+              <a>readit</a>
             </Link>
-            <span className="text-2xl font-semibold">
-              <Link href="/">
-                <a>readit</a>
-              </Link>
-            </span>
-          </div>
-          <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-            <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
-            <input
-              type="text"
-              className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
-            />
-          </div>
+          </span>
+        </div>
+        <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+          <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+          <input
+            type="text"
+            className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+          />
+        </div>
+        <div className="flex">
+          <Link href="/login">
+            <a className="w-32 py-1 mr-4 button blue hollow">Log In</a>
+          </Link>
+          <Link href="/register">
+            <a className="w-32 py-1 button blue">Sign Up</a>
+          </Link>
         </div>
       </div>
     </Fragment>
