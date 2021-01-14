817b7fc3b91c88d7bda5c866db26a0c1ebb856ac
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-14 16:21:57 +0530

    Starting progress on header of main posts card
    at homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { Post } from "../types";
+
+dayjs.extend(relativeTime);
 
 function PageIndex() {
   const [posts, setPosts] = useState<Post[]>([]);
@@ -31,7 +36,34 @@ function PageIndex() {
                   <p>V</p>
                 </div>
                 <div className="w-full p-2">
-                  <p>{post.body}</p>
+                  <div className="flex items-center">
+                    <Link href={`/r/${post.subName}`}>
+                      <a className="flex items-center">
+                        <img
+                          src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+                          className="w-6 h-6 mr-1 rounded-full"
+                        />
+                        <span className="text-xs font-bold hover:underline">
+                          /r/{post.subName}
+                        </span>
+                      </a>
+                    </Link>
+                    <p className="text-xs text-gray-500">
+                      <span className="mx-1">•</span>
+                      Posted by
+                      <Link href={`/u/user`}>
+                        <a className="mx-1 hover:underline">/u/user</a>
+                      </Link>
+                      <span>•</span>
+                      <Link
+                        href={`/r/${post.subName}/${post.identifier}/${post.slug}`}
+                      >
+                        <a className="mx-1 hover:underline">
+                          {dayjs(post.createdAt).fromNow()}
+                        </a>
+                      </Link>
+                    </p>
+                  </div>
                 </div>
               </div>
             ))}
