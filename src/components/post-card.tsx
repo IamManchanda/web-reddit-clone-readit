9d063c98112e95a2a9dc5ed46dcbe2666cf8d1f9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-15 18:50:30 +0530

    Post Card Component

---
+++ src/components/post-card.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Post } from "../types";
+
+dayjs.extend(relativeTime);
+
+interface PostCardProps {
+  post: Post;
+}
+
+const PostCard: React.FC<PostCardProps> = ({ post }) => (
+  <div key={post.identifier} className="flex mb-4 bg-white rounded">
+    <div className="w-10 text-center bg-gray-200 rounded-l">
+      <p>&nbsp;</p>
+    </div>
+    <div className="w-full p-2">
+      <div className="flex items-center">
+        <Link href={`/r/${post.subName}`}>
+          <a className="flex items-center">
+            <img
+              src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+              className="w-6 h-6 mr-1 rounded-full"
+            />
+            <span className="text-xs font-bold hover:underline">
+              /r/{post.subName}
+            </span>
+          </a>
+        </Link>
+        <p className="text-xs text-gray-500">
+          <span className="mx-1">•</span>
+          Posted by
+          <Link href={`/u/${post.username}`}>
+            <a className="mx-1 hover:underline">/u/{post.username}</a>
+          </Link>
+          <span>•</span>
+          <Link href={post.url}>
+            <a className="mx-1 hover:underline">
+              {dayjs(post.createdAt).fromNow()}
+            </a>
+          </Link>
+        </p>
+      </div>
+      <Link href={post.url}>
+        <a className="my-1 text-lg font-medium">{post.title}</a>
+      </Link>
+      {post.body && <p className="my-1 text-sm">{post.body}</p>}
+      <div className="flex">
+        <Link href={post.url}>
+          <a>
+            <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+              <i className="mr-1 fas fa-comment-alt fa-xs" />
+              <span className="font-bold">20 Comments</span>
+            </div>
+          </a>
+        </Link>
+        <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+          <i className="mr-1 fas fa-share fa-xs" />
+          <span className="font-bold">Share</span>
+        </div>
+        <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+          <i className="mr-1 fas fa-bookmark fa-xs" />
+          <span className="font-bold">Save</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default PostCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from "next/head";
 import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import { Post } from "../types";
-
-dayjs.extend(relativeTime);
+import PostCard from "../components/post-card";
 
 function PageIndex() {
   const [posts, setPosts] = useState<Post[]>([]);
@@ -31,63 +27,7 @@ function PageIndex() {
         <div className="container flex pt-4">
           <div className="w-160">
             {posts.map((post) => (
-              <div key={post.identifier} className="flex mb-4 bg-white rounded">
-                <div className="w-10 text-center bg-gray-200 rounded-l">
-                  <p>&nbsp;</p>
-                </div>
-                <div className="w-full p-2">
-                  <div className="flex items-center">
-                    <Link href={`/r/${post.subName}`}>
-                      <a className="flex items-center">
-                        <img
-                          src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
-                          className="w-6 h-6 mr-1 rounded-full"
-                        />
-                        <span className="text-xs font-bold hover:underline">
-                          /r/{post.subName}
-                        </span>
-                      </a>
-                    </Link>
-                    <p className="text-xs text-gray-500">
-                      <span className="mx-1">•</span>
-                      Posted by
-                      <Link href={`/u/${post.username}`}>
-                        <a className="mx-1 hover:underline">
-                          /u/{post.username}
-                        </a>
-                      </Link>
-                      <span>•</span>
-                      <Link href={post.url}>
-                        <a className="mx-1 hover:underline">
-                          {dayjs(post.createdAt).fromNow()}
-                        </a>
-                      </Link>
-                    </p>
-                  </div>
-                  <Link href={post.url}>
-                    <a className="my-1 text-lg font-medium">{post.title}</a>
-                  </Link>
-                  {post.body && <p className="my-1 text-sm">{post.body}</p>}
-                  <div className="flex">
-                    <Link href={post.url}>
-                      <a>
-                        <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                          <i className="mr-1 fas fa-comment-alt fa-xs" />
-                          <span className="font-bold">20 Comments</span>
-                        </div>
-                      </a>
-                    </Link>
-                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                      <i className="mr-1 fas fa-share fa-xs" />
-                      <span className="font-bold">Share</span>
-                    </div>
-                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                      <i className="mr-1 fas fa-bookmark fa-xs" />
-                      <span className="font-bold">Save</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <PostCard post={post} key={post.identifier} />
             ))}
           </div>
         </div>
