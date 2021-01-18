5a3370673804a62bb6a6027e38b1b9bbe3eb2182
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-18 21:22:22 +0530

    Add sub title, name & styling changes

--- src/pages/r/[sub].tsx
+++ src/pages/r/[sub].tsx
@@ -52,15 +52,25 @@ function PageSub() {
               )}
             </div>
             <div className="h-20 bg-white">
-              <div className="container flex">
-                <Image
-                  src={sub.imageUrl}
-                  alt="Sub"
-                  title="Sub"
-                  className="rounded-full"
-                  width={80}
-                  height={80}
-                />
+              <div className="container relative flex">
+                <div className="absolute" style={{ top: -15 }}>
+                  <Image
+                    src={sub.imageUrl}
+                    alt="Sub"
+                    title="Sub"
+                    className="rounded-full"
+                    width={70}
+                    height={70}
+                  />
+                </div>
+                <div className="pt-1 pl-24">
+                  <div className="flex items-center">
+                    <h1 className="mb-1 text-3xl font-bold">{sub.title}</h1>
+                  </div>
+                  <p className="text-sm font-bold text-gray-500">
+                    /r/{sub.name}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
