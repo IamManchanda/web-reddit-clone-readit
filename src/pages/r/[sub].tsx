5b6fa1bff2ab5eea0d77cd9827f5a629d1c8579d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-18 19:34:54 +0530

    Banner in Subreddit page

--- src/pages/r/[sub].tsx
+++ src/pages/r/[sub].tsx
@@ -3,11 +3,12 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import PostCard from "../../components/post-card";
+import { Sub } from "../../types";
 
 function PageSub() {
   const router = useRouter();
   const { sub: subName } = router.query;
-  const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : "");
+  const { data: sub, error } = useSWR<Sub>(subName ? `/subs/${subName}` : "");
 
   if (error) {
     router.push("/");
@@ -31,9 +32,28 @@ function PageSub() {
       <Head>
         <title>{sub?.title}</title>
       </Head>
-      <div className="container flex pt-5">
-        {sub && <div className="w-160">{postsMarkup}</div>}
-      </div>
+      {sub && (
+        <Fragment>
+          <div className="bg-blue-500">
+            {sub.bannerUrl ? (
+              <div
+                className="h-56 bg-blue-500"
+                style={{
+                  backgroundImage: `url(${sub.bannerUrl})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              />
+            ) : (
+              <div className="h-20 bg-blue-500" />
+            )}
+          </div>
+          <div className="container flex pt-5">
+            <div className="w-160">{postsMarkup}</div>
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 }
