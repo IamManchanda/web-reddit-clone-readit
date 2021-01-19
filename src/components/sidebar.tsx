4a66047a6f1e15a657503d58d8332aa9ba4b6841
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-20 01:05:59 +0530

    Add Created Date & "Create Post Button on Auth"
    in Sidebar component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,15 @@
+import dayjs from "dayjs";
 import { Sub } from "../types";
+import { useAuthState } from "../context/auth";
+import Link from "next/link";
 
 interface SidebarProps {
   sub: Sub;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ sub }) => {
+  const { authenticated } = useAuthState();
+
   return (
     <div className="ml-6 w-80">
       <div className="bg-white rounded">
@@ -23,6 +28,15 @@ const Sidebar: React.FC<SidebarProps> = ({ sub }) => {
               <p>online</p>
             </div>
           </div>
+          <p className="my-3">
+            <i className="mr-2 fas fa-birthday-cake" />
+            Created {dayjs(sub.createdAt).format("D MMM YYYY")}
+          </p>
+          {authenticated && (
+            <Link href={`/r/${sub.name}/submit`}>
+              <a className="w-full py-1 text-sm blue button">Create Post</a>
+            </Link>
+          )}
         </div>
       </div>
     </div>
