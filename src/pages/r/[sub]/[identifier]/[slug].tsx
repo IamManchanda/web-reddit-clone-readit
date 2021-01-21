4dcd70281f69b15d5ddf9d7ef01dcdf914d92e1d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-21 20:51:54 +0530

    More changes in "Sub Identifier Slug" Page

--- src/pages/r/[sub]/[identifier]/[slug].tsx
+++ src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { Post } from "../../../../types";
 
 function PageSubIdentifierSlug() {
   const router = useRouter();
   const { identifier, sub, slug } = router.query;
-  const { data: post, error } = useSWR(
+  const { data: post, error } = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null,
   );
 
@@ -19,6 +21,22 @@ function PageSubIdentifierSlug() {
       <Head>
         <title>{post?.title}</title>
       </Head>
+      <Link href={`/r/${sub}`}>
+        <a>
+          <div className="flex items-center w-full h-20 p-8 bg-blue-500">
+            <div className="container flex">
+              {post && (
+                <div className="w-8 h-8 mr-2 overflow-hidden rounded-full">
+                  <Image src={post.sub.imageUrl} width={32} height={32} />
+                </div>
+              )}
+              <p className="text-xl font-semibold text-white hover:underline">
+                /r/{sub}
+              </p>
+            </div>
+          </div>
+        </a>
+      </Link>
     </>
   );
 }
