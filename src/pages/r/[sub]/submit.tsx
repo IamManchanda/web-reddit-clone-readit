18539174c51844622c69e19cb22671f9442f5ead
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-22 23:03:14 +0530

    Create "Sub => Submit" Page

---
+++ src/pages/r/[sub]/submit.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function PageSubSubmit() {
+  return (
+    <>
+      <Head>
+        <title>Sub: Submit Post</title>
+      </Head>
+
+      <h1 className="mt-4 text-3xl font-bold text-center">Sub: Submit Post</h1>
+    </>
+  );
+}
+
+export default PageSubSubmit;
