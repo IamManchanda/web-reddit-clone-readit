784aa30f7d4d39e5f5f11e1bcb9cce1fcfda3b25
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-24 00:24:41 +0530

    Redirect to login page (server side) if not auth

--- src/pages/r/[sub]/submit.tsx
+++ src/pages/r/[sub]/submit.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
@@ -90,5 +91,29 @@ function PageSubSubmit() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const { cookie } = req.headers;
+
+    if (!cookie) {
+      throw new Error("Missing auth token cookie");
+    }
+
+    await axios.get("/auth/me", {
+      headers: { cookie },
+    });
+
+    return {
+      props: {},
+    };
+  } catch (error) {
+    res
+      .writeHead(307, {
+        Location: "/login",
+      })
+      .end();
+  }
+};
 
 export default PageSubSubmit;
