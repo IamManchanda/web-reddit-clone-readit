86b46a81b3816917234c1bbcf2a60f5c2376c703
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-24 00:03:30 +0530

    Add Post & Comment Submissions in username page

--- src/pages/u/[username].tsx
+++ src/pages/u/[username].tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import PostCard from "../../components/post-card";
+import { Comment, Post } from "../../types";
 
 function PageUsername() {
   const router = useRouter();
@@ -25,7 +28,42 @@ function PageUsername() {
       {data && (
         <div className="container flex pt-5">
           <div className="w-160">
-            <p>{data.user.username}</p>
+            {data.submissions.map((submission: any) => {
+              if (submission.type === "Post") {
+                const post: Post = submission;
+                return <PostCard key={post.identifier} post={post} />;
+              } else {
+                const comment: Comment = submission;
+                return (
+                  <div
+                    key={comment.identifier}
+                    className="flex my-4 bg-white rounded"
+                  >
+                    <div className="flex-shrink-0 w-10 py-4 text-center bg-gray-200 rounded-l">
+                      <i className="text-gray-500 fas fa-comment-alt fa-xs" />
+                    </div>
+                    <div className="w-full p-2">
+                      <p className="mb-2 text-xs text-gray-500">
+                        <span>{comment.username}</span>
+                        <span>&nbsp;commented on&nbsp;</span>
+                        <Link href={comment.post.url}>
+                          <a className="font-semibold cursor-pointer hover:underline">
+                            {comment.post.title}
+                          </a>
+                        </Link>
+                        <span className="mx-1">•</span>
+                        <Link href={`/r/${comment.post.subName}`}>
+                          <a className="text-black cursor-pointer hover:underline">
+                            /r/{comment.post.subName}
+                          </a>
+                        </Link>
+                      </p>
+                      <p>{comment.body}</p>
+                    </div>
+                  </div>
+                );
+              }
+            })}
           </div>
         </div>
       )}
