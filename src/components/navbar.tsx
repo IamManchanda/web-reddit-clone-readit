c7bfb5d449422c3f72461cc73d9ddb88705c016d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-24 18:39:14 +0530

    Working Progress in Searching a Sub in Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 import RedditLogo from "../assets/images/reddit.svg";
 import { useAuthState, useAuthDispatch } from "../context/auth";
+import { Sub } from "../types";
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState("");
+  const [subs, setSubs] = useState<Sub[]>([]);
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
 
@@ -12,6 +17,17 @@ const Navbar: React.FC = () => {
       await axios.get("/auth/logout");
       dispatch("LOGOUT");
       window.location.reload();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const searchSubs = async (subName: string) => {
+    setName(subName);
+
+    try {
+      const { data } = await axios.get(`/subs/search/${subName}`);
+      setSubs(data);
     } catch (error) {
       console.log({ error });
     }
@@ -31,13 +47,41 @@ const Navbar: React.FC = () => {
           </Link>
         </span>
       </div>
-      <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      <div className="relative flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search" />
         <input
           type="text"
           placeholder="Search..."
           className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+          value={name}
+          onChange={(event) => searchSubs(event.target.value)}
         />
+        <div
+          className="absolute left-0 right-0 bg-white"
+          style={{
+            top: "100%",
+          }}
+        >
+          {subs?.map((sub, index) => (
+            <div
+              key={index}
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+            >
+              <Image
+                src={sub.imageUrl}
+                className="rounded-full"
+                alt="Sub"
+                title="Sub"
+                width={32}
+                height={32}
+              />
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex">
         {!loading &&
